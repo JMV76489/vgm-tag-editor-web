383e3d31bc6e9e41d86824f02684c032f5323409
JMV76489 <95047311+JMV76489@users.noreply.github.com>
2026-02-09 18:45:18 -0600

    feat: add VGMTagEditorApp component

---
+++ src/VGMTagEditorApp.tsx
@@ -0,0 +1,13 @@
+import './VGMTagEditorApp.css'
+import { VGMDataViewer } from './components/vgm-data-viewer/VGMDataViewer';
+
+export const VGMTagEditorApp = () => {
+
+  return (
+    <div>
+      <VGMDataViewer />
+    </div>
+  )
+}
+
+export default VGMTagEditorApp
