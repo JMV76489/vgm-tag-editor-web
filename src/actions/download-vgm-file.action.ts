9e085d72dead46692e24d17989cba62c65f91241
JMV76489 <95047311+JMV76489@users.noreply.github.com>
2026-04-24 12:30:43 -0600

    feat: add download VGM file action

---
+++ src/actions/download-vgm-file.action.ts
@@ -0,0 +1,18 @@
+export const downloadVGMFileAction = (vgmFileData: Uint8Array<ArrayBuffer>, vgmFileName: string) => {
+    const vgmFileBlob = new Blob([vgmFileData], {
+        type: "application/octet-stream",
+    });
+
+    const url = URL.createObjectURL(vgmFileBlob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', vgmFileName);
+    link.style.display = 'none';
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link)
+    URL.revokeObjectURL(url);
+}
